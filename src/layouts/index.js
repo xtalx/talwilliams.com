b47e1127aa933d75c3d3863bd4948fa579ba7ce3
xtalx <polygonchorus@gmail.com>
2017-11-13 13:31:04 -0800

    less

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -17,16 +17,7 @@ const TemplateWrapper = ({ children }) => (
       <link rel="icon" href={favicon} />
       <html lang="en" />
     </Helmet>
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    <div>{children()}</div>
   </div>
 )
 
