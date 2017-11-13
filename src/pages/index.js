0458de6e38c4ebc0aec33d3bbf9797a7d0e12e09
xtalx <polygonchorus@gmail.com>
2017-11-12 20:26:53 -0800

    oops

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const IndexPage = () => (
   <div>
-    <h1 style={{ textAlign: center }}>Something, something, something. </h1>
+    <h1 style={{ textAlign: 'center' }}>Something, something, something. </h1>
   </div>
 )
 
