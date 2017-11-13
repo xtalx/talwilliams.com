1bb0d739ea7d616119b2a2749e8bbfccf738748f
xtalx <polygonchorus@gmail.com>
2017-11-12 20:23:24 -0800

    placeholder

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,35 +5,6 @@ import Helmet from 'react-helmet'
 import favicon from '../images/favicon.ico'
 import './index.css'
 
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
-
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
@@ -46,7 +17,6 @@ const TemplateWrapper = ({ children }) => (
       <link rel="icon" href={favicon} />
       <html lang="en" />
     </Helmet>
-    <Header />
     <div
       style={{
         margin: '0 auto',
